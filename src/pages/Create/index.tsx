f95673637f7ddf6199b3d3f5283f6906dbc228c3
Kevin Jung <kevinjung13@gmail.com>
2023-04-15 15:29:41 +0700

    Added States for Input, rendering of results for both Create and Mint sections
    Added States for Input, rendering of results for both Create and Mint sections
    - Remaining: To study Rainbowkit and try to implement

--- src/pages/Create/index.tsx
+++ src/pages/Create/index.tsx
@@ -12,13 +12,45 @@ import {
 } from '@chakra-ui/react';
 
 /* Example of Data from API */
-const resultsData = {
+export const resultsData = {
   name: 'Tripping Duck',
   imageURL:
     'https://images-platform.99static.com/pULAgn-AED8QzzPGS40V0GCDOEk=/0x0:1000x1000/500x500/top/smart/99designs-contests-attachments/130/130378/attachment_130378088',
 };
   
-/* Components for Results */
+export default function Create() {
+
+  /* Loading State for button */
+  const [isLoading, setIsLoading] = useState(false)
+   /* State for badge */
+  const [inputValue, setInputValue] = useState("")
+  /* State for create section and results */
+  const [create, setCreate] = useState(true);
+  const [showResults, setShowResults] = useState(false);
+
+  /* handleInputValue function for Badge */
+  const handleInputValue = () => {
+    const newValue = 'Tripping Duck';
+    setInputValue(prevValue => prevValue + newValue);
+  };
+
+  /* HandleClick function for Create Button */
+  const handleClick = () => {
+    setIsLoading(true)
+    setTimeout(() => {
+      setIsLoading(false);
+      setCreate(false);
+      setShowResults(true);
+    }, 2000)
+  }
+
+  /* HandleCreatePage function for Try Again Button */
+  const handleCreatePage = () => {
+    setShowResults(false);
+    setCreate(true);
+  }
+
+/* Elements for Results */
 function Results() {
   
   return (
@@ -26,7 +58,7 @@ function Results() {
       {/* Container for whole page  */}
       <Box textAlign="center" color={'white'}>
         {/* Result title */}
-        <Heading as="h2" size="xl" mt={6} mb={2}>
+        <Heading as="h2" size="xl" pt={6} mb={2}>
           Results for: "{resultsData.name}"
         </Heading>
         {/* Stacking of Image */}
@@ -47,11 +79,13 @@ function Results() {
           direction={'row'}
           spacing={5}
           mt={'8'}
+          pb={'8'}
           align={'center'}
           justify={'center'}>
           {/* Try Again Button */}
           <Link href={'/create'}>
             <Button
+              onClick={handleCreatePage}
               size={'lg'}
               rounded={'lg'}
               bg={'pink.400'}
@@ -76,101 +110,74 @@ function Results() {
     </>
   )
 }
-  
-export default function Create() {
 
-  /* Loading State for button */
-  const [isLoading, setIsLoading] = useState(false)
-
-   /* State for badge */
-  const [inputValue, setInputValue] = useState("")
-
-  /* State for results */
-  const [showCreator, setShowCreator] = useState(true);
-  const [showResults, setShowResults] = useState(false);
-
-  /* handleInputValue function for Badge */
-  const handleInputValue = () => {
-    const newValue = 'Tripping Duck';
-    setInputValue(prevValue => prevValue + newValue);
-  };
-
-  /* HandleClick function for Create Button */
-  const handleClick = () => {
-    setIsLoading(true)
-    setTimeout(() => {
-      setIsLoading(false);
-      setShowCreator(false);
-      setShowResults(true);
-    }, 2000)
-
-  }
-
-  /* Rendering of Results and Creator */
-  // {showResults && <Results />}
-  // {showCreator && <Creator />}
-
+  /* Elements for Create */
   return (
     <>
-      {/* Container for whole page */}
-      <Flex
-      minH={'100vh'}
-      align={'center'}
-      justify={'center'}
-      color={'white'}>
-        {/* Stacking of content */}
-        <Stack
-          spacing={7}
-          h={{ base: '100%', md: '100%' }}
-          w={{ base: '85%', md: '100%' }}
-          maxW={'lg'}
-          bg={'gray.700'}
-          rounded={'xl'}
-          boxShadow={'lg'}
-          align={'center'}
-          p={9}>
-          {/* Heading */}
-          <Heading
-            lineHeight={1.1}
-            fontSize={{ base: '2xl', md: '3xl' }}
-            textAlign={'center'}>
-            Create Your NFT
-          </Heading>
-          {/* Form */}
-          <FormControl>
-            <Input
-              type={'text'}
-              size={{ base: 'md', md: 'lg' }}
-              borderColor={'white'}
-              placeholder="Type what you want to create..."
-              _placeholder={{ color: 'gray.400' }}
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              _hover={{ borderColor: 'cyan.600' }}/>
-          </FormControl>
-          {/* Stacking of Button */}
-          <Stack spacing={6}>
-            <Button
-              bg={'pink.400'}
-              color={'white'}
-              isLoading={isLoading}
-              loadingText={'Creating'}
-              onClick={handleClick}
-              _hover={{ bg: 'yellow.500' }}>
-              Create
-            </Button>
-          </Stack>
-          <Button
-            onClick={handleInputValue}
-            rounded={"full"}
-            variant={'outline'}
-            color={'none'}
-            _hover={{
-              bg: 'gray.400'}}>
-            Tripping Duck
-          </Button>
-        </Stack>
-      </Flex>
+      {showResults && <Results />}
+      {create && (
+        <>
+          {/* Container for whole page */ }
+          <Flex
+            minH={'100vh'}
+            align={'center'}
+            justify={'center'}
+            color={'white'}>
+            {/* Stacking of content */}
+            <Stack
+              spacing={7}
+              h={{ base: '100%', md: '100%' }}
+              w={{ base: '85%', md: '100%' }}
+              maxW={'lg'}
+              bg={'gray.700'}
+              rounded={'xl'}
+              boxShadow={'lg'}
+              align={'center'}
+              p={9}>
+              {/* Heading */}
+              <Heading
+                lineHeight={1.1}
+                fontSize={{ base: '2xl', md: '3xl' }}
+                textAlign={'center'}>
+                Create Your NFT
+              </Heading>
+              {/* Form */}
+              <FormControl>
+                <Input
+                  type={'text'}
+                  size={{ base: 'md', md: 'lg' }}
+                  borderColor={'white'}
+                  placeholder="Type what you want to create..."
+                  _placeholder={{ color: 'gray.400' }}
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  _hover={{ borderColor: 'cyan.600' }} />
+              </FormControl>
+              {/* Stacking of Button */}
+              <Stack spacing={6}>
+                <Button
+                  bg={'pink.400'}
+                  color={'white'}
+                  isLoading={isLoading}
+                  loadingText={'Creating'}
+                  onClick={handleClick}
+                  _hover={{ bg: 'yellow.500' }}>
+                  Create
+                </Button>
+              </Stack>
+              <Button
+                bg={'blue.500'}
+                onClick={handleInputValue}
+                rounded={"full"}
+                variant={'solid'}
+                color={'none'}
+                _hover={{
+                  bg: 'yellow.500'}}>
+                Tripping Duck
+              </Button>
+            </Stack>
+          </Flex>
+        </> )}
     </>
   );
 }
